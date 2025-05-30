d7e1258deca2749d2f5788fe955b0435867e24c7
Christian <christian.moise77@free.fr>
2025-05-30 09:54:20 +0200

    Use all_files_dict[st_size] as a list instead of dict.

--- deja_vu.py
+++ deja_vu.py
@@ -56,15 +56,17 @@ def parse_directory(path, all_files_dict, **kwargs):
             st_dev = entry.stat(follow_symlinks=False).st_dev
 
             if not all_files_dict.get(st_size):
-                all_files_dict[st_size] = {}
+                all_files_dict[st_size] = []
 
-            all_files_dict[st_size][entry.path] = {
-                "st_ino": st_ino,
-                "st_dev": st_dev,
-                "safe": False,
-                "to_delete": False,
-                "checked": False,
-            }
+            all_files_dict[st_size].append(
+                {
+                    "path": entry.path,
+                    "st_ino": st_ino,
+                    "st_dev": st_dev,
+                    "safe": False,
+                    "to_delete": False,
+                }
+            )
 
         elif entry.is_dir():
             parse_directory(entry.path, all_files_dict, **kwargs)
@@ -82,55 +84,57 @@ for dir_path in args.dirs:
     )
 
 for file_size in all_files:
-    paths_same_size = all_files[file_size]
+    paths_same_size_list = all_files[file_size]
 
-    if len(paths_same_size) == 1:
+    if len(paths_same_size_list) == 1:
         continue
 
-    for path1 in paths_same_size:
-        data_path1 = paths_same_size[path1]
+    for i1 in range(len(paths_same_size_list)):
+        path1_dict = paths_same_size_list[i1]
 
-        if data_path1["to_delete"]:
+        if path1_dict["to_delete"]:
             continue
 
-        data_path1["checked"] = True
+        for i2 in range(i1 + 1, len(paths_same_size_list), 1):
+            path2_dict = paths_same_size_list[i2]
 
-        for path2 in paths_same_size:
-            data_path2 = paths_same_size[path2]
-
-            if data_path2["to_delete"] or data_path2["checked"]:
+            if path2_dict["to_delete"]:
                 continue
 
-            if data_path1["safe"] and data_path2["safe"]:
+            if path1_dict["safe"] and path2_dict["safe"]:
                 continue
 
-            if data_path1["st_ino"] == data_path2["st_ino"] and \
-                   data_path1["st_dev"] == data_path2["st_dev"]:
+            if path1_dict["st_ino"] == path2_dict["st_ino"] and \
+                   path1_dict["st_dev"] == path2_dict["st_dev"]:
                 continue
 
-            if not filecmp.cmp(path1, path2, False):
+            if not filecmp.cmp(path1_dict["path"], path2_dict["path"], False):
                 continue
 
             if not args.rm:
-                print(f"sum '{path1}' '{path2}'")
+                print(f"sum \"{path1_dict['path']}\" \"{path2_dict['path']}\"")
             else:
-                file_to_rm = None
+                file_to_rm_dict = None
 
-                if data_path1["safe"]:
-                    file_to_rm = path2
-                elif data_path2["safe"]:
-                    file_to_rm = path1
+                if path1_dict["safe"]:
+                    file_to_rm_dict = path2_dict
+                elif path2_dict["safe"]:
+                    file_to_rm_dict = path1_dict
                 elif args.rm_regex:
-                    if re.search(r"%s" % args.rm_regex, path1):
-                        file_to_rm = path1
-                    elif re.search(r"%s" % args.rm_regex, path2):
-                        file_to_rm = path2
+                    if re.search(r"%s" % args.rm_regex, path1_dict["path"]):
+                        file_to_rm_dict = path1_dict
+                    elif re.search(r"%s" % args.rm_regex, path2_dict["path"]):
+                        file_to_rm_dict = path2_dict
 
-                if not file_to_rm:
-                    file_to_rm = path2
-                file_to_safe = path1 if file_to_rm == path2 else path2
+                if not file_to_rm_dict:
+                    file_to_rm_dict = path2_dict
+                file_to_safe_dict = path1_dict if file_to_rm_dict == path2_dict else path2_dict
 
-                print(f"rm '{file_to_rm}'")
-                print(f"   # safe '{file_to_safe}'")
-                paths_same_size[file_to_safe]["safe"] = True
-                paths_same_size[file_to_rm]["to_delete"] = True
+                file_to_rm_dict["to_delete"] = True
+                file_to_safe_dict["safe"] = True
+
+                print(f"rm \"{file_to_rm_dict['path']}\"")
+                print(f"   # safe \"{file_to_safe_dict['path']}\"")
+
+                if path1_dict == file_to_rm_dict:
+                    break
